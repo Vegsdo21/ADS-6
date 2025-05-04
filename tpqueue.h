b29773810a7634ceaf6634d04560a53dcfb21dc5
KorobovaKate <kat3kor@yandex.ru>
2025-05-04 15:28:22 +0300

    Update tpqueue.h

--- tpqueue.h
+++ tpqueue.h
@@ -1,10 +1,8 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+// Файл include/tpqueue.h
 
 template<typename T>
 class TPQueue {
- private:
+private:
     struct Node {
         T data;
         Node* next;
@@ -12,8 +10,7 @@ class TPQueue {
     };
     Node* head;
     Node* tail;
-    
- public:
+public:
     TPQueue() : head(nullptr), tail(nullptr) {}
     ~TPQueue() {
         while (head) {
@@ -38,6 +35,7 @@ class TPQueue {
         }
         newNode->next = current->next;
         current->next = newNode;
+        
         if (!newNode->next) {
             tail = newNode;
         }
@@ -59,10 +57,3 @@ class TPQueue {
         return head == nullptr;
     }
 };
-
-struct SYM {
-  char ch;
-  int prior;
-};
-
-#endif  // INCLUDE_TPQUEUE_H_
